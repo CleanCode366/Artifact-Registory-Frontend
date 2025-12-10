085954f5cfe460f0434891a2716f793e547a97c1
sarthaksam003 <sarthaksam003@gmail.com>
2025-12-10 11:57:18 +0530

    added config folder with env.ts

--- src/views/Upload/Upload.tsx
+++ src/views/Upload/Upload.tsx
@@ -1,9 +1,11 @@
-import InscriptionUploader from '@/components/Uploader0.2/InscriptionUploader0.31';
+// import InscriptionUploader from '@/components/Uploader0.2/InscriptionUploader0.31';
+// import InscriptionUploader from '@/components/Uploader0.2/InscriptionUploader0.31';
+import EnhancedInscriptionUploader from '@/components/Uploader0.3/uploader0.3/InscriptionUploader0.4';
 import React from 'react';
 
 const Upload: React.FC = () => {
   return (
-      <InscriptionUploader/>
+      <EnhancedInscriptionUploader/>
   );
 };
 
